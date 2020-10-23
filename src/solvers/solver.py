b0242b2f2c382a33b5bbdc7c1a18661688682d98
rhermoso <rhermoso@unizar.es>
2020-10-23 08:25:21 +0200

    Errors in L255 solver.py

--- src/solvers/solver.py
+++ src/solvers/solver.py
@@ -228,6 +228,9 @@ class Solver:
                 result_dict[frozenset(subset[1])] = subset[0]
         return result_dict
 
+    def append_coordinates(self, lat, lon):
+        return str(lat)+','+str(lon)
+
     def get_all_subsets_heuristic(self, data, geographic_distances):
         """
         Calculates all the possible subsets for the given data. Takes the set maximum length for subsets into account.
@@ -244,11 +247,13 @@ class Solver:
             for v in values:
                 candidates_set.add(v)
     
+        candidates_list = list(candidates_set)
         print('+++++++ Values: ', candidates_set)
     
         print('***** Longitud antes: ', len(data))
     
-        data = [x for x in data if (str(x.coordinates.x)+","+str(x.coordinates.y)) in candidates_set] 
+        data = [x for x in data if self.append_coordinates(x.coordinates.x, x.coordinates.y) in candidates_list] 
+        # data = [x for x in data if '48.869173,2.391884' in candidates_list] 
         # [x for x in somelist if not determine(x)]                
         print('***** Longitud después: ', len(data))
         
